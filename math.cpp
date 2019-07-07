af0eba6243e510b30a0a42258993730cef3927e7
pranjal Jain <prnjljn@gmail.com>
2019-07-07 20:48:51 +0530

    Math added

---
+++ math.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int binaryExponentiation(int x,int n)
+{
+    int result=1;
+    while(n>0)
+    {
+        if(n % 2 ==1)
+            result=result * x;
+        x=x*x;
+        n=n/2;
+    }
+    return result;
+}
+int modularExponentiation(int x,int n,int M)
+{
+    int result=1;
+    while(n>0)
+    {
+        if(power % 2 ==1)
+            result=(result * x)%M;
+        x=(x*x)%M;
+        n=n/2;
+    }
+    return result;
+}
+int GCD(int A, int B) {
+    if(B==0)
+        return A;
+    else
+        return GCD(B, A % B);
+}
+int extendedEuclid(int a, int b, int & x, int & y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
+int main(){
+    
+}
